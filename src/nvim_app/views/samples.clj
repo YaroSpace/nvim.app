a5824f676de5dd57a9fc19e2de445a0e7bd8fa59
Yaro <yaro@dream-it.es>
2025-08-16 22:54:28 +0300

    feat: ui

---
+++ src/nvim_app/views/samples.clj
@@ -0,0 +1,28 @@
+(ns nvim-app.views.samples)
+
+(def sample-plugins
+  [{:url "https://github.com/NStefan002/15puzzle.nvim"
+    :description "Classic 15 puzzle for Neovim."
+    :name "15puzzle.nvim"
+    :topics ["game" "lua" "neovim" "neovim-plugin" "plugin" "puzzle-game"]
+    :stars "6"
+    :created "2024-03-17"
+    :updated "2024-03-31"
+    :category "game"}
+   {:url "https://github.com/Abstract-IDE/Abstract"
+    :description "neovim as an IDE"
+    :name "Abstract"
+    :topics ["neovim" "neovim-config" "ide" "vim" "vscode" "abstract-ide"]
+    :stars "202"
+    :created "1970-01-01"
+    :updated "2023-12-01"
+    :category "game"}
+   {:url "https://github.com/0styx0/abbreinder.nvim"
+    :description "Abbreviation reminder plugin for Neovim 0.5+"
+    :name "abbreinder.nvim"
+    :topics ["neovim" "lua" "vim" "vim-abbreviations" "neovim-plugin"]
+    :stars "90"
+    :created "2021-07-11"
+    :updated "2022-04-28"
+    :category "todo"}])
+
